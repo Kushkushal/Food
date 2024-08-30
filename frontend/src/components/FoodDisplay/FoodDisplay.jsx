b13eb759296b1ba6c259a337bb3625a0ca38853f
Demo436 <kushalgowda@gmail.com>
2024-08-30 14:11:46 +0530

    first commit

--- frontend/src/components/FoodDisplay/FoodDisplay.jsx
+++ frontend/src/components/FoodDisplay/FoodDisplay.jsx
@@ -4,6 +4,7 @@ import './FoodDisplay.css'
 import { StoreContext } from '../../context/StoreContext'
 import FoodItem from '../FoodItem/FoodItem'
 
+// eslint-disable-next-line react/prop-types
 const FoodDisplay = ({category}) => {
     const {food_list} = useContext(StoreContext)
   return (
