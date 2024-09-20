532b27f1e01ce4739de1ba4aa90d7b29d9c75e05
Kushal Gowda <136258142+Kushkushal@users.noreply.github.com>
2024-09-20 17:53:43 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,11 +9,14 @@ import Footer from './components/Footer/Footer'
 import LoginPopup from './components/LopinPopup/LoginPopup'
 import Verify from './pages/Verify/Verify'
 import MyOrders from './pages/MyOrders/MyOrders'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   const [showLogin,setShowLogin] = useState(false)
   return (
     <>
+    <ToastContainer />
     {showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
       <div className='app'>
         <Navbar setShowLogin={setShowLogin} />
